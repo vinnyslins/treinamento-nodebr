45f9b6ca87af5dec053bb6aa027c7fcfabacca62
Vinnys Lins <vlins@orion.tech>
2018-12-31 18:09:58 -0200

    Resolvendo promises do módulo de sincronia de funções com async/await

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -34,35 +34,57 @@ function obterEndereco(idUsuario, callback) {
   }, 2000);
 }
 
-const usuarioPromise = obterUsuario();
-usuarioPromise.then(usuario => {
-  return obterTelefone(usuario.id).then(resultado => {
-    return {
-      usuario: {
-        nome: usuario.nome,
-        id: usuario.id
-      },
-      telefone: resultado
-    }
-  });
-}).then(resultado => {
-  const endereco = obterEnderecoAsync(resultado.usuario.id);
-  return endereco.then((result) => {
-    return {
-      usuario: resultado.usuario,
-      telefone: resultado.telefone,
-      endereco: result
-    }
-  });
-}).then(resultado => {
-  console.log(`
-    Nome: ${resultado.usuario.nome}
-    Endereço: ${resultado.endereco.rua}, ${resultado.endereco.numero}
-    Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
-  `);
-}).catch(erro => {
-  console.error('deu ruim', erro);
-});
+async function main() {
+  try {
+    const usuario = await obterUsuario();
+    const resultado = await Promise.all([
+      obterTelefone(usuario.id),
+      obterEnderecoAsync(usuario.id)
+    ]);
+    const telefone = resultado[0];
+    const endereco = resultado[1];
+
+    console.log(`
+      Nome: ${usuario.nome}
+      Telefone: (${telefone.ddd}) ${telefone.telefone}
+      Endereço: ${endereco.rua}, ${endereco.numero}
+    `);
+  } catch (erro) {
+    console.error('deu ruim', erro);
+  }
+}
+
+main();
+
+// const usuarioPromise = obterUsuario();
+// usuarioPromise.then(usuario => {
+//   return obterTelefone(usuario.id).then(resultado => {
+//     return {
+//       usuario: {
+//         nome: usuario.nome,
+//         id: usuario.id
+//       },
+//       telefone: resultado
+//     }
+//   });
+// }).then(resultado => {
+//   const endereco = obterEnderecoAsync(resultado.usuario.id);
+//   return endereco.then((result) => {
+//     return {
+//       usuario: resultado.usuario,
+//       telefone: resultado.telefone,
+//       endereco: result
+//     }
+//   });
+// }).then(resultado => {
+//   console.log(`
+//     Nome: ${resultado.usuario.nome}
+//     Endereço: ${resultado.endereco.rua}, ${resultado.endereco.numero}
+//     Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+//   `);
+// }).catch(erro => {
+//   console.error('deu ruim', erro);
+// });
 
 // obterUsuario((erro, usuario) => {
 //   if (erro) {
