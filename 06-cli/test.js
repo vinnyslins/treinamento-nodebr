edacea1bf5d654a421fef466a06b1294bb912052
Vinnys Lins <vinnysfl@hotmail.com>
2019-01-04 02:08:19 -0200

    Criando métodos listar e cadastrar no módulo de manipulação de arquivos

---
+++ 06-cli/test.js
@@ -0,0 +1,26 @@
+const { deepEqual, ok } = require('assert');
+
+const database = require('./database');
+
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: 'Flash',
+  poder: 'Speed',
+  id: 1
+};
+
+describe('Suíte de manipulação de heróis', () => {
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+  });
+  it('Deve pesquisar um herói usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const [resultado] = await database.listar(expected.id);
+    deepEqual(resultado, expected);
+  });
+  it('Deve cadastrar um herói, usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+    deepEqual(actual, expected);
+  });
+});
