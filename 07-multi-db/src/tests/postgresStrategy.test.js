e31aebe98ba26fcdd48382433e5beb60446429a7
Vinnys Lins <vinnysfl@hotmail.com>
2019-01-09 03:30:24 -0200

    Refatorando a estratégia do Postgres para multi-schemas

--- 07-multi-db/src/tests/postgresStrategy.test.js
+++ 07-multi-db/src/tests/postgresStrategy.test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
-const Postgres = require('../db/strategies/postgres');
+const Postgres = require('../db/strategies/postgres/postgres');
+const HeroiSchema = require('../db/strategies/postgres/schemas/heroiSchema');
 const Context = require('../db/strategies/base/contextStrategy');
 
-const context = new Context(new Postgres());
 const MOCK_HEROI_CADASTRAR = {
   nome: 'Gavião Negro',
   poder: 'Flechas'
@@ -12,10 +12,14 @@ const MOCK_HEROI_ATUALIZAR = {
   poder: 'Dinheiro'
 };
 
+let context = {};
+
 describe('Postgres strategy', function () {
   this.timeout(Infinity);
   this.beforeAll(async () => {
-    await context.connect();
+    const connection = await Postgres.connect();
+    const model = await Postgres.defineModel(connection, HeroiSchema);
+    context = new Context(new Postgres(connection, model));
     await context.delete();
     await context.create(MOCK_HEROI_ATUALIZAR);
   });
